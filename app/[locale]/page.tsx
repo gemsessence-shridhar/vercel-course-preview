09ca253e2f8f576885fe48466ef1465946e726fe
Shridhar Gupta <shridhar.gupta@gemsessence.com>
2025-01-17 13:43:26 +0530

    Setup intl

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 import {useTranslations} from 'next-intl';
 
 export default function Home() {
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import withNextIntl from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const config = withNextIntl()(nextConfig);
+
+export default config;
