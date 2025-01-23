7992d869ed5cf046d19311283387a4589fd16c7e
Shridhar Gupta <shridhar.gupta@gemsessence.com>
2025-01-23 11:53:25 +0530

    Comment subtile for now

--- src/components/shared/hooks/useVideoSubtitles.js
+++ src/components/shared/hooks/useVideoSubtitles.js
@@ -17,66 +17,66 @@ const mapSubtitles = (subtitleCmsId, data) => {
 }
 
 export const useVideoSubTitles = async (subtitleCmsId) => {
-  const client = createApolloClient();
+  // const client = createApolloClient();
 
-  const enUsSubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'en-us',}, fetchPolicy: 'network-only' },
-  );
+  // const enUsSubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'en-us',}, fetchPolicy: 'network-only' },
+  // );
 
-  const deDeSubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'fr-fr',}, fetchPolicy: 'network-only' },
-  );
+  // const deDeSubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'fr-fr',}, fetchPolicy: 'network-only' },
+  // );
 
-  const frFrSubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'de-de',}, fetchPolicy: 'network-only' },
-  );
+  // const frFrSubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'de-de',}, fetchPolicy: 'network-only' },
+  // );
 
-  const zhCnSubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'zh-cn',}, fetchPolicy: 'network-only' },
-  );
+  // const zhCnSubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'zh-cn',}, fetchPolicy: 'network-only' },
+  // );
 
-  const ptBrSubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'pt-br',}, fetchPolicy: 'network-only' },
-  );
+  // const ptBrSubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'pt-br',}, fetchPolicy: 'network-only' },
+  // );
 
-  const es419SubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'es-419',}, fetchPolicy: 'network-only' },
-  );
+  // const es419SubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'es-419',}, fetchPolicy: 'network-only' },
+  // );
 
-  const itItSubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'it-it',}, fetchPolicy: 'network-only' },
-  );
+  // const itItSubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'it-it',}, fetchPolicy: 'network-only' },
+  // );
 
-  const trTrSubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'tr-tr',}, fetchPolicy: 'network-only' },
-  );
+  // const trTrSubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'tr-tr',}, fetchPolicy: 'network-only' },
+  // );
 
-  const jaJpSubtitleData = await client.query({
-    query: pagePreview.queries.GET_VIDEO_SUBTITLE,
-    variables: {subtitleCmsId: subtitleCmsId, locale: 'ja-jp',}, fetchPolicy: 'network-only' },
-  );
+  // const jaJpSubtitleData = await client.query({
+  //   query: pagePreview.queries.GET_VIDEO_SUBTITLE,
+  //   variables: {subtitleCmsId: subtitleCmsId, locale: 'ja-jp',}, fetchPolicy: 'network-only' },
+  // );
 
-  const  subtitles = mapSubtitles(subtitleCmsId, [
-    {...enUsSubtitleData.data, locale: 'en-us'},
-    {...deDeSubtitleData.data, locale: 'de-de'},
-    {...frFrSubtitleData.data, locale: 'fr-fr'},
-    {...zhCnSubtitleData.data, locale: 'zh-cn'},
-    {...ptBrSubtitleData.data, locale: 'pt-br'},
-    {...es419SubtitleData.data, locale: 'es-419'},
-    {...itItSubtitleData.data, locale: 'it-it'},
-    {...trTrSubtitleData.data, locale: 'tr-tr'},
-    {...jaJpSubtitleData.data, locale: 'ja-jp'},
-  ])
+  // const  subtitles = mapSubtitles(subtitleCmsId, [
+  //   {...enUsSubtitleData.data, locale: 'en-us'},
+  //   {...deDeSubtitleData.data, locale: 'de-de'},
+  //   {...frFrSubtitleData.data, locale: 'fr-fr'},
+  //   {...zhCnSubtitleData.data, locale: 'zh-cn'},
+  //   {...ptBrSubtitleData.data, locale: 'pt-br'},
+  //   {...es419SubtitleData.data, locale: 'es-419'},
+  //   {...itItSubtitleData.data, locale: 'it-it'},
+  //   {...trTrSubtitleData.data, locale: 'tr-tr'},
+  //   {...jaJpSubtitleData.data, locale: 'ja-jp'},
+  // ])
   return {
-    subtitles,
+    subtitles: [],
     loading: false,
   }
 }
