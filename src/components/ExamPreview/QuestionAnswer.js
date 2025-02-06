41fecdb5a4af7a8529a1e3d29a5beee77b10b784
Shridhar Gupta <shridhar.gupta@gemsessence.com>
2025-02-06 16:24:49 +0530

    fix lint issue

--- src/components/ExamPreview/QuestionAnswer.js
+++ src/components/ExamPreview/QuestionAnswer.js
@@ -1,6 +1,5 @@
 "use client"
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Row, Col } from 'react-bootstrap';
 import { isEmpty } from 'lodash';
